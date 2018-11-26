9a89841bea3fc6e0b1fcf407428c37d85218a238
jcoope44 <jcoope44@kent.edu>
2018-11-26 09:49:27 -0500

    done

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import county
 import colorMap
 
 all_counties = readFile.get_counties()
-colors = ['red', 'blue', 'green', 'yellow']
+
+color_string = input('Enter colors to use in a comma seperated list (red,blue,green,...): ')
+colors = color_string.replace(' ', '').split(',')
 
 for key in all_counties:
     all_counties[key].possible_colors = colors.copy()
 
 success = colorMap.color_map(colors, all_counties)
-if success:
-    print('success')
-else:
-    print('failed :(')
-
+while not success:
+    color_string = input('Not enough colors added. Add more: ')
+    colors.extend(color_string.replace(' ', '').split(','))
+    for key in all_counties:
+        all_counties[key].possible_colors = colors.copy()
+    success = colorMap.color_map(colors, all_counties)
+    
 for key in all_counties:
         if hasattr(all_counties[key], 'color'):
             print(key, ': ', all_counties[key].color)
